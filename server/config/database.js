390cf852fd6f8cf8bf7b9108b251f61f870098a1
Kevalya Gupta <kevalyagupta20033006@gmail.com>
2025-04-14 22:16:51 +0530

    INITIAL FILES

--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const { MONGODB_URL } = process.env;
+const { MONGODB_URI } = process.env;
 
 exports.connect = () => {
-	mongoose
-		.connect(MONGODB_URL, {
-			useNewUrlparser: true,
-			useUnifiedTopology: true,
-		})
-		.then(console.log(`DB Connection Success`))
-		.catch((err) => {
-			console.log(`DB Connection Failed`);
-			console.log(err);
-			process.exit(1);
-		});
+  mongoose
+    .connect(MONGODB_URI, {
+      useNewUrlParser: true, // Correct spelling here
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("DB Connection Success");
+    })
+    .catch((err) => {
+      console.log("DB Connection Failed");
+      console.log(err);
+      process.exit(1); // Exit the process if the DB connection fails
+    });
 };
--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,6 +1,11 @@
-const Razorpay = require("razorpay");
+// const Razorpay = require("razorpay");
 
-exports.instance = new Razorpay({
-	key_id: process.env.RAZORPAY_KEY,
-	key_secret: process.env.RAZORPAY_SECRET,
-});
+// const instance = new Razorpay({
+//     key_id: process.env.RAZORPAY_KEY,
+//     key_secret: process.env.RAZORPAY_SECRET,
+// });
+
+// module.exports = instance;
+
+// TEMPORARY MOCK FOR DEVELOPMENT
+module.exports = {};
